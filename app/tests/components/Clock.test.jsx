b9d13a2b8ce3f85fad63cfd3f8094e001351b092
Dan Goldberg <d.goldberg82@gmail.com>
2016-09-05 15:44:16 -0400

    Added Clock Styles and Tests

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var ReactDom = require('react-Dom');
+var ReactDom = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
@@ -9,6 +9,17 @@ var Clock = require('Clock');
 describe('Clock', () => {
   it('should exist', () => {
     expect(Clock).toExist();
+  });
+
+  describe('render', () => {
+    it('should render clock to output', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
+      // converts component to HTML and passes it through
+      var $el = $(ReactDom.findDOMNode(clock));
+      var actualText = $el.find('.clock-text').text();
+
+      expect(actualText).toBe('01:02');
+    });
   });
 
   describe('formatSeconds', () => {
